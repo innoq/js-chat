e640492dc11cb476d7fdd1092dcdf7586be2f5db
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 15:24:51 +0200

    assignment 3.4: functions as object properties

--- dist/script.js
+++ dist/script.js
@@ -5,52 +5,57 @@
 
     const m1 = {
         textBody: "Lisa enters the chat",
-        type: TYPE_SYSTEM
+        type: TYPE_SYSTEM,
+        render: renderMessage
     };
 
     const m2 = {
         textBody: "Paul enters the chat",
-        type: TYPE_SYSTEM
+        type: TYPE_SYSTEM,
+        render: renderMessage
     };
 
     const m3 = {
         sender: "Paul",
         textBody: "Hello!",
-        type: TYPE_USER
+        type: TYPE_USER,
+        render: renderMessage
     };
 
     const m4 = {
         sender: "Lisa",
         textBody: "Hello Paul! How are you?",
-        type: TYPE_USER
+        type: TYPE_USER,
+        render: renderMessage
     };
 
     const m5 = {
         sender: "Paul",
         textBody: "Hi Lisa, i'm fine, thanks. How are you?",
-        type: TYPE_USER
+        type: TYPE_USER,
+        render: renderMessage
     };
 
-    function renderMessage(message) {
+    function renderMessage() {
         let formattedMessage = "";
 
-        if (message.type === TYPE_SYSTEM) {
-            formattedMessage = "..." + message.textBody + "...";
+        if (this.type === TYPE_SYSTEM) {
+            formattedMessage = "..." + this.textBody + "...";
         } else {
-            formattedMessage = `${message.sender}: ${message.textBody}`;
+            formattedMessage = `${this.sender}: ${this.textBody}`;
         }
 
         return formattedMessage;
     }
 
-    function sendMessage(message, renderFunction) {
+    function sendMessage(message) {
         console.log(message);
-        renderFunction && console.log(renderFunction(message));
+        message.render && console.log(message.render());
     }
 
-    sendMessage(m1, renderMessage);
-    sendMessage(m2, renderMessage);
-    sendMessage(m3, renderMessage);
-    sendMessage(m4, renderMessage);
-    sendMessage(m5, renderMessage);
+    sendMessage(m1);
+    sendMessage(m2);
+    sendMessage(m3);
+    sendMessage(m4);
+    sendMessage(m5);
 })();
