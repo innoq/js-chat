f91c9e39bedc8a0eec1d3746896cf90f80701a33
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 13:37:28 +0200

    assignment 2.1: message objects

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hooray, my first line of JavaScript!");
+const m1 = {
+    textBody: "Lisa enters the chat",
+    type: "system"
+};
+
+const m2 = {
+    textBody: "Paul enters the chat",
+    type: "system"
+};
+
+const m3 = {
+    sender: "Paul",
+    textBody: "Hello!",
+    type: "user"
+};
+
+const m4 = {
+    sender: "Lisa",
+    textBody: "Hello Paul! How are you?",
+    type: "user"
+};
+
+const m5 = {
+    sender: "Paul",
+    textBody: "Hi Lisa, i'm fine, thanks. How are you?",
+    type: "user"
+};
