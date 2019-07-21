08961281ea9c3f580e030f62f84335129edecfa9
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 23:06:46 +0200

    assignment 9.2: serving static files with express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("Hooray! My first Node.js program!");
+const express = require("express");
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static("frontend"));
+
+app.listen(PORT, function(){
+    console.log("backend listening on " + PORT);
+});
