084c0ee527aec7b73249f293b4837096414fd759
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 22:16:23 +0200

    assignment 8.3: using await and async

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ window.addEventListener("load", () => {
 
     const messageForm = document.querySelector("#messageForm");
 
-    messageForm.addEventListener("submit", (event) => {
+    messageForm.addEventListener("submit", async (event) => {
         event.preventDefault();
 
         const userNameInput = document.querySelector("#username");
@@ -126,10 +126,9 @@ window.addEventListener("load", () => {
         const userName = userNameInput.value;
         const messageText = messageInput.value;
 
-        delay(1000).then(() => {
-            chat.sendMessage(new UserMessage(messageText, userName));
-        });
-
         messageInput.value = "";
+
+        await delay(1000);
+        chat.sendMessage(new UserMessage(messageText, userName));
     });
 });
