366b403554768883bef4ade6b4993b1f73691aaf
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 13:38:36 +0200

    assignment 2.2: message type constants

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+const TYPE_SYSTEM = "system";
+const TYPE_USER = "user";
+
 const m1 = {
     textBody: "Lisa enters the chat",
-    type: "system"
+    type: TYPE_SYSTEM
 };
 
 const m2 = {
     textBody: "Paul enters the chat",
-    type: "system"
+    type: TYPE_SYSTEM
 };
 
 const m3 = {
     sender: "Paul",
     textBody: "Hello!",
-    type: "user"
+    type: TYPE_USER
 };
 
 const m4 = {
     sender: "Lisa",
     textBody: "Hello Paul! How are you?",
-    type: "user"
+    type: TYPE_USER
 };
 
 const m5 = {
     sender: "Paul",
     textBody: "Hi Lisa, i'm fine, thanks. How are you?",
-    type: "user"
+    type: TYPE_USER
 };
