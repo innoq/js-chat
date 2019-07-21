5ac34f240dccfda1ba50b1df6445c6a7e2e552ff
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 23:09:43 +0200

    assignment 9.3: a GET request with a JSON response

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.static("frontend"));
 
+const messages = [];
+
+const getMessages = (req, res) => {
+    res.status(200).json(messages);
+};
+
+app.get("/api/messages", getMessages);
+
 app.listen(PORT, function(){
     console.log("backend listening on " + PORT);
 });
