857f3a212cb4aedae273ef18aeaf154d6e674f32
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 17:17:11 +0200

    assignment 4.1: an array of objects

--- dist/script.js
+++ dist/script.js
@@ -5,11 +5,13 @@
 
     const createMessage = (textBody, type, sender) => ({textBody, type, sender, render: renderMessage});
 
-    const m1 = createMessage("Lisa enters the chat", TYPE_SYSTEM);
-    const m2 = createMessage("Paul enters the chat", TYPE_SYSTEM);
-    const m3 = createMessage("Hello!", TYPE_USER, "Paul");
-    const m4 = createMessage("Hello Paul! How are you?", TYPE_USER, "Lisa");
-    const m5 = createMessage("Hi Lisa, i'm fine, thanks. How are you?", TYPE_USER, "Paul");
+    const messages = [
+        createMessage("Lisa enters the chat", TYPE_SYSTEM),
+        createMessage("Paul enters the chat", TYPE_SYSTEM),
+        createMessage("Hello!", TYPE_USER, "Paul"),
+        createMessage("Hello Paul! How are you?", TYPE_USER, "Lisa"),
+        createMessage("Hi Lisa, i'm fine, thanks. How are you?", TYPE_USER, "Paul")
+    ];
 
     function renderMessage() {
         let formattedMessage = "";
@@ -28,9 +30,9 @@
         message.render && console.log(message.render());
     }
 
-    sendMessage(m1);
-    sendMessage(m2);
-    sendMessage(m3);
-    sendMessage(m4);
-    sendMessage(m5);
+    sendMessage(messages[0]);
+    sendMessage(messages[1]);
+    sendMessage(messages[2]);
+    sendMessage(messages[3]);
+    sendMessage(messages[4]);
 })();
