3990a490395e34649fec83d743bf897982aa580f
tbse <17569011+tbse@users.noreply.github.com>
2019-07-21 15:30:16 +0200

    assignment 3.5: arrow functions

--- dist/script.js
+++ dist/script.js
@@ -3,38 +3,13 @@
     const TYPE_SYSTEM = "system";
     const TYPE_USER = "user";
 
-    const m1 = {
-        textBody: "Lisa enters the chat",
-        type: TYPE_SYSTEM,
-        render: renderMessage
-    };
+    const createMessage = (textBody, type, sender) => ({textBody, type, sender, render: renderMessage});
 
-    const m2 = {
-        textBody: "Paul enters the chat",
-        type: TYPE_SYSTEM,
-        render: renderMessage
-    };
-
-    const m3 = {
-        sender: "Paul",
-        textBody: "Hello!",
-        type: TYPE_USER,
-        render: renderMessage
-    };
-
-    const m4 = {
-        sender: "Lisa",
-        textBody: "Hello Paul! How are you?",
-        type: TYPE_USER,
-        render: renderMessage
-    };
-
-    const m5 = {
-        sender: "Paul",
-        textBody: "Hi Lisa, i'm fine, thanks. How are you?",
-        type: TYPE_USER,
-        render: renderMessage
-    };
+    const m1 = createMessage("Lisa enters the chat", TYPE_SYSTEM);
+    const m2 = createMessage("Paul enters the chat", TYPE_SYSTEM);
+    const m3 = createMessage("Hello!", TYPE_USER, "Paul");
+    const m4 = createMessage("Hello Paul! How are you?", TYPE_USER, "Lisa");
+    const m5 = createMessage("Hi Lisa, i'm fine, thanks. How are you?", TYPE_USER, "Paul");
 
     function renderMessage() {
         let formattedMessage = "";
