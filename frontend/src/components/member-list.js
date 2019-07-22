5d6e7c0a5d18e1102b13bf2521b12c135dd96e7e
tbse <17569011+tbse@users.noreply.github.com>
2019-07-22 13:47:02 +0200

    assignment 12.2: update the member-list

--- frontend/src/components/member-list.js
+++ frontend/src/components/member-list.js
@@ -1,13 +1,25 @@
+import {getMessages} from "../services/messages.js";
+import Chat from "../modules/chat.js";
+
 class MemberList extends HTMLElement {
     connectedCallback() {
         const headerElement = document.createElement("h2");
         headerElement.innerHTML = "Members";
 
-        const ulElement = document.createElement("ul");
-        ulElement.innerHTML = "<li>Dummy</li>";
+        this.ulElement = document.createElement("ul");
+        this.ulElement.innerHTML = "<li>Dummy</li>";
 
         this.appendChild(headerElement);
-        this.appendChild(ulElement);
+        this.appendChild(this.ulElement);
+
+        this.updateMembers();
+        setInterval(() => this.updateMembers(), 1000);
+    }
+
+    async updateMembers() {
+        const messages = await getMessages();
+        const members = Chat.extractMembers(messages);
+        this.ulElement.innerHTML = members.map(member => `<li>${member}</li>`).join("");
     }
 }
 
